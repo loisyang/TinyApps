b0de9aba20ea36a1924a214aac1fbb1fc5dda6d4
Jingqi Yang <jingqiy@andrew.cmu.edu>
2017-01-09 17:13:45 -0500

    fix pagination

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', 'lastPage'],
     menu: '#menu',
     continuousVertical: false,
     continuousHorizontal: false,
