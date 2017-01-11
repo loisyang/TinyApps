a2521c34dc79e97751b6f8b691b4cc2a5e7bda94
Jingqi Yang <jingqiy@andrew.cmu.edu>
2017-01-11 12:27:36 -0500

    transition for each section: left - move up , right - move down. switch between active and inactive. 2. add a new product section

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
+
   $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', 'lastPage'],
+    anchors: ['firstPage', 'secondPage', 'thirdPage','lastPage'],
     menu: '#menu',
     continuousVertical: false,
     continuousHorizontal: false,
     scrollHorizontally: false,
     'navigation': true,
     'navigationPosition': 'right',
-    'navigationTooltips': ['Intro', 'Letterix','About'],
+    'navigationTooltips': ['Intro', 'Letterix','123Tiles','About'],
     showActiveTooltip: true,
-  });
-});
+    keyboardscrolling:true,
+
+    onLeave: function(index, nextIndex, direction){
+      var thisSection = "#section"+ (index-1)
+      $(thisSection+ ' div.words').removeClass('active').addClass('inactive');
+      $(thisSection+ ' img.screenshot').removeClass('active').addClass('inactive');
+
+      // detect the next section
+      if(direction == 'down') {
+        var nextSection = "#section"+index;
+      } else if (direction == 'up') {
+        var nextSection = "#section"+(index-2);
+      }
+      console.log(nextSection)
+      if (nextSection != ('section0')){
+        $(nextSection + ' div.words').removeClass('inactive').addClass('active');
+        $(nextSection + ' img.screenshot').removeClass('inactive').addClass('active');
+      }
+        // else if(index == 2 && direction == 'up'){
+        //     alert("Going to section 1!");
+        // }
+    // }
+    }
+  })
+})
+
+
