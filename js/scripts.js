e6aa5737062d5068afe36f13dbe359fbdd029bde
Jingqi Yang <jingqiy@andrew.cmu.edu>
2017-03-23 17:27:07 -0400

    solve resolution problem

--- js/scripts.js
+++ js/scripts.js
@@ -25,27 +25,81 @@ $(window).on("load",function(){
     loopBottom: true,
     fixedElements: '#header',
 
-    // onLeave: function(index, nextIndex, direction){
-    //   var thisSection = "#section"+ (index-1)
-    //   $(thisSection+ ' div.words').removeClass('active').addClass('inactive');
-    //   // $(thisSection+ ' img.screenshot').removeClass('active').addClass('inactive');
-
-    //   // detect the next section
-    //   if(direction == 'down') {
-    //     var nextSection = "#section"+index;
-    //   } else if (direction == 'up') {
-    //     var nextSection = "#section"+(index-2);
-    //   }
-    //   console.log(nextSection)
-    //   if (nextSection != ('section0')){
-    //     $(nextSection + ' div.words').removeClass('inactive').addClass('active');
-    //     // $(nextSection + ' img.screenshot').removeClass('inactive').addClass('active');
-    //   }
-    //     // else if(index == 2 && direction == 'up'){
-    //     //     alert("Going to section 1!");
-    //     // }
-    // // }
-    // }
   })
 });
+
+// Add a custom video player control
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
+
+// var mediaPlayer;
+// function initialiseMediaPlayer(){
+//     mediaPlayer = document.getElementById('media-video');
+//     mediaPlayer.controls = true
+// }
+
+// function togglePlayPause() {
+//    var btn = document.getElementById('play-pause-button');
+//    if (mediaPlayer.paused || mediaPlayer.ended) {
+//       btn.title = 'pause';
+//       btn.innerHTML = 'pause';
+//       btn.className = 'pause';
+//       mediaPlayer.play();
+//    }
+//    else {
+//       btn.title = 'play';
+//       btn.innerHTML = 'play';
+//       btn.className = 'play';
+//       mediaPlayer.pause();
+//    }
+// }
+
+// function changeButtonType(btn, value) {
+//    btn.title = value;
+//    btn.innerHTML = value;
+//    btn.className = value;
+// }
+
+// function stopPlayer() {
+//    mediaPlayer.pause();
+//    mediaPlayer.currentTime = 0;
+// }
+
+// function changeVolume(direction) {
+//    if (direction === '+') mediaPlayer.volume += mediaPlayer.volume == 1 ? 0 : 0.1;
+//    else mediaPlayer.volume -= (mediaPlayer.volume == 0 ? 0 : 0.1);
+//    mediaPlayer.volume = parseFloat(mediaPlayer.volume).toFixed(1);
+// }
+
+// function toggleMute() {
+//    var btn = document.getElementById('mute-button');
+//    if (mediaPlayer.muted) {
+//       changeButtonType(btn, 'mute');
+//       mediaPlayer.muted = false;
+//    }
+//    else {
+//       changeButtonType(btn, 'unmute');
+//       mediaPlayer.muted = true;
+//    }
+// }
+
+// function replayMedia() {
+//    resetPlayer();
+//    mediaPlayer.play();
+// }
+
+
+// mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
+// function updateProgressBar() {
+//    var progressBar = document.getElementById('progress-bar');
+//    var percentage = Math.floor((100 / mediaPlayer.duration) *
+//    mediaPlayer.currentTime);
+//    progressBar.value = percentage;
+//    progressBar.innerHTML = percentage + '% played';
+// }
+
+// function resetPlayer() {
+//    progressBar.value = 0;
+//    mediaPlayer.currentTime = 0;
+//    changeButtonType(playPauseBtn, 'play');
+// }
 
